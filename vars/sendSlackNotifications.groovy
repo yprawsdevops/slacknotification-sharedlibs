658b39694aed11cc10b330c73a2065ffa0cdc461
Srinu-web <112843932+Srinu-web@users.noreply.github.com>
2022-10-24 14:52:16 +0530

    Update sendSlackNotifications.groovy

--- vars/sendSlackNotifications.groovy
+++ vars/sendSlackNotifications.groovy
@@ -1,3 +1,5 @@
+@Library('sharedlibraryysr') _
+
 def call(String buildStatus = 'STARTED') {
   // build status of null means successful
   //This is the condition which we are checking weather buildStatus is SUCCESSFULL or not.
